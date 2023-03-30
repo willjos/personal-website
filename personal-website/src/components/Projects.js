bd46e120458096867a8506fc43cf3dde27b6c795
Will <trainee.william.simms@sigmalabs.co.uk>
2023-03-30 13:58:28 +0100

    selected slides display dynamic title and description content

--- personal-website/src/components/Projects.js
+++ personal-website/src/components/Projects.js
@@ -8,8 +8,22 @@ import {
   ImageWithZoom,
 } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
+import { useState } from "react";
+import { ironImageDescriptions } from "./ironImageDescriptions";
 
 export default function Projects() {
+  const [selectedSlideIndex, setSelectedSlideIndex] = useState(0);
+
+  const handleSlideChange = (action) => {
+    let newSlideIndex = selectedSlideIndex;
+    if (action === "Next") {
+      newSlideIndex += 1;
+    } else if (action === "Back") {
+      newSlideIndex -= 1;
+    }
+    setSelectedSlideIndex(newSlideIndex);
+  };
+
   return (
     <div className="projects-wrapper p-5 mb-2 mx-2 bg-green text-white">
       <p className="text-2xl mb-1 font-bold"> Projects </p>
@@ -26,46 +40,66 @@ export default function Projects() {
             viewed, including workout history, personal records, and progress
             graphs.
           </p>
-          <CarouselProvider
-            className="mt-4"
-            naturalSlideHeight={19.5}
-            naturalSlideWidth={9}
-            visibleSlides={1}
-            totalSlides={9}
-            hasMasterSpinner
-          >
-            <Slider className="w-1/3">
-              <Slide index={0}>
-                <ImageWithZoom src="IRON - Login.jpg" />
-              </Slide>
-              <Slide index={1}>
-                <ImageWithZoom src="IRON - Homepage.jpg" />
-              </Slide>
-              <Slide index={2}>
-                <ImageWithZoom src="IRON - Start session.jpg" />
-              </Slide>
-              <Slide index={3}>
-                <ImageWithZoom src="IRON - B autocomplete.jpg" />
-              </Slide>
-              <Slide index={4}>
-                <ImageWithZoom src="IRON - Lat autocomplete.jpg" />
-              </Slide>
-              <Slide index={5}>
-                <ImageWithZoom src="IRON - Bench press log.jpg" />
-              </Slide>
-              <Slide index={6}>
-                <ImageWithZoom src="IRON - Graphs.jpg" />
-              </Slide>
-              <Slide index={7}>
-                <ImageWithZoom src="IRON - Personal Records.jpg" />
-              </Slide>
-              <Slide index={8}>
-                <ImageWithZoom src="IRON - Workout history.jpg" />
-              </Slide>
-            </Slider>
-            <ButtonBack className="my-2 mr-2 p-1 bg-blue">Back</ButtonBack>
-            <ButtonNext className="my-2 ml-32 p-1 bg-blue">Next</ButtonNext>
-          </CarouselProvider>
+          <div className="flex justify-around mt-4">
+            <CarouselProvider
+              className="h-2/5 w-2/5 mr-3"
+              naturalSlideHeight={200}
+              naturalSlideWidth={100}
+              visibleSlides={1}
+              totalSlides={9}
+              hasMasterSpinner
+            >
+              <Slider className="">
+                <Slide index={0}>
+                  <ImageWithZoom src="IRON - Login.jpg" />
+                </Slide>
+                <Slide index={1}>
+                  <ImageWithZoom src="IRON - Homepage.jpg" />
+                </Slide>
+                <Slide index={2}>
+                  <ImageWithZoom src="IRON - Start session.jpg" />
+                </Slide>
+                <Slide index={3}>
+                  <ImageWithZoom src="IRON - Bench press log.jpg" />
+                </Slide>
+                <Slide index={4}>
+                  <ImageWithZoom src="IRON - B autocomplete.jpg" />
+                </Slide>
+                <Slide index={5}>
+                  <ImageWithZoom src="IRON - Lat autocomplete.jpg" />
+                </Slide>
+                <Slide index={6}>
+                  <ImageWithZoom src="IRON - Graphs.jpg" />
+                </Slide>
+                <Slide index={7}>
+                  <ImageWithZoom src="IRON - Personal Records.jpg" />
+                </Slide>
+                <Slide index={8}>
+                  <ImageWithZoom src="IRON - Workout history.jpg" />
+                </Slide>
+              </Slider>
+              <ButtonBack
+                className="my-2 p-1 bg-blue"
+                onClick={() => handleSlideChange("Back")}
+              >
+                Back
+              </ButtonBack>
+              <ButtonNext
+                className="my-2 p-1 bg-blue"
+                onClick={() => handleSlideChange("Next")}
+              >
+                Next
+              </ButtonNext>
+            </CarouselProvider>
+            <div className="w-3/5 text-left ml-3 p-1">
+              <p className="font-semibold">
+                {ironImageDescriptions[selectedSlideIndex].title}
+              </p>
+              <p className="mt-1">
+                {ironImageDescriptions[selectedSlideIndex].description}
+              </p>
+            </div>
+          </div>
         </div>
         <div className="pl-4">
           <p className="text-lg font-semibold my-1">
